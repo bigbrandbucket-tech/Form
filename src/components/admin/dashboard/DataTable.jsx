8de319fa002c485fe263763fdd1a0c47676136dd
A G <Anubh896@gmail.com>
2024-07-28 10:42:01 +0530

    hide

--- src/components/admin/dashboard/DataTable.jsx
+++ src/components/admin/dashboard/DataTable.jsx
@@ -146,6 +146,13 @@ export default function DataTable() {
       return y - fontSize - 5; // Adjust y position for the next line
     };
 
+    const addTextIfExists = (page, label, value, y) => {
+      if (value) {
+        return addText(page, label, value, y);
+      }
+      return y; // Return the same y if value doesn't exist
+    };
+
     const addText2 = (page, title, value, y) => {
       const fontSize = 16;
       const font = page.font;
@@ -180,19 +187,19 @@ export default function DataTable() {
     let { page: page1, y: y1} = createPage("Personal Information");
     y1 = addText2(page1, "Personal Details", "", y1);
     y1 -= 10;
-    y1 = addText(page1, "First Name", data.firstName, y1);
-    y1 = addText(page1, "Middle Name", data.middleName, y1);
-    y1 = addText(page1, "Last Name", data.lastName, y1);
-    y1 = addText(page1, "Email Address", data.emailAddress, y1);
-    //   y1 = addText(page1, 'Email address (re-enter)', data.emailAddressReEnter, y1);
-    y1 = addText(page1, "Phone", data.phone, y1);
-    //   y1 = addText(page1, 'Phone (re-enter)', data.phoneReEnter, y1);
-    y1 = addText(page1, "Date Of Birth", data.dateOfBirth, y1);
-    y1 = addText(page1, "Gender", data.gender, y1);
-    y1 = addText(page1, "Country Of Birth", data.countryOfBIrth, y1);
-    y1 = addText(page1, "City Of Birth", data.cityOfBirth, y1);
-    y1 = addText(page1, "Marital Status", data.maritalStatus, y1);
-    y1 = addText(
+    y1 = addTextIfExists(page1, "First Name", data.firstName, y1);
+    y1 = addTextIfExists(page1, "Middle Name", data.middleName, y1);
+    y1 = addTextIfExists(page1, "Last Name", data.lastName, y1);
+    y1 = addTextIfExists(page1, "Email Address", data.emailAddress, y1);
+    //   y1 = addTextIfExists(page1, 'Email address (re-enter)', data.emailAddressReEnter, y1);
+    y1 = addTextIfExists(page1, "Phone", data.phone, y1);
+    //   y1 = addTextIfExists(page1, 'Phone (re-enter)', data.phoneReEnter, y1);
+    y1 = addTextIfExists(page1, "Date Of Birth", data.dateOfBirth, y1);
+    y1 = addTextIfExists(page1, "Gender", data.gender, y1);
+    y1 = addTextIfExists(page1, "Country Of Birth", data.countryOfBIrth, y1);
+    y1 = addTextIfExists(page1, "City Of Birth", data.cityOfBirth, y1);
+    y1 = addTextIfExists(page1, "Marital Status", data.maritalStatus, y1);
+    y1 = addTextIfExists(
       page1,
       "Preferred language to contact you",
       data.preferredLanguage,
@@ -201,90 +208,90 @@ export default function DataTable() {
     y1 -= 10;
     y1 = addText2(page1, "Applicant Status", "", y1);
     y1 -= 10;
-    y1 = addText(
+    y1 = addTextIfExists(
       page1,
       "Are you applying on behalf of someone?",
       data.applyingForYourselfOrSomeoneElse == 1 ? "Yes" : "No",
       y1
     );
-    y1 = addText(page1, "I am? ", data.iam, y1);
-    y1 = addText(
+    y1 = addTextIfExists(page1, "I am? ", data.iam, y1);
+    y1 = addTextIfExists(
       page1,
       "Surname(s) / last name(s) ",
       data.applicantSurname,
       y1
     );
-    y1 = addText(
+    y1 = addTextIfExists(
       page1,
       "Given name(s) / first name(s)",
       data.applicantGivenName,
       y1
     );
-    y1 = addText(page1, " Mailing address", data.applicantMailingAddress, y1);
-    y1 = addText(page1, "Phone Extension", data.applicantPhoneExt, y1);
-    y1 = addText(page1, "Phone Number", data.applicantPhone, y1);
+    y1 = addTextIfExists(page1, " Mailing address", data.applicantMailingAddress, y1);
+    y1 = addTextIfExists(page1, "Phone Extension", data.applicantPhoneExt, y1);
+    y1 = addTextIfExists(page1, "Phone Number", data.applicantPhone, y1);
 
     y1 -= 10;
     //   let { page: page2, y: y2 } = createPage('Passport Details');
     y1 = addText2(page1, "Passport Details", "", y1);
     y1 -= 10;
-    y1 = addText(page1, "Passport No", data.passportNo, y1);
-    y1 = addText(
+    y1 = addTextIfExists(page1, "Passport No", data.passportNo, y1);
+    y1 = addTextIfExists(
       page1,
       "Passport number (re-enter)",
       data.passportNumberReEnter,
       y1
     );
-    y1 = addText(page1, "Passport Issue Date", data.passportIssueDate, y1);
-    y1 = addText(page1, "Passport Expiry Date", data.passportExpiryDate, y1);
-    y1 = addText(
+    y1 = addTextIfExists(page1, "Passport Issue Date", data.passportIssueDate, y1);
+    y1 = addTextIfExists(page1, "Passport Expiry Date", data.passportExpiryDate, y1);
+    y1 = addTextIfExists(
       page1,
       "Passport Country/Nationality",
       data.passportCountryNationality,
       y1
     );
 
-    y1 = addText(page1, "Taiwan Identification Number", data?.TIN || "", y1);
+    y1 = addTextIfExists(page1, "Taiwan Identification Number", data?.TIN || "", y1);
 
     y1 -= 10;
     //   let { page: page3, y: y3 } = createPage('Residential Address');
     y1 = addText2(page1, "Residential Address", "", y1);
     y1 -= 10;
-    y1 = addText(page1, "Street Address/name", data.streetAddressName, y1);
-    y1 = addText(
+    y1 = addTextIfExists(page1, "Street Address/name", data.streetAddressName, y1);
+    y1 = addTextIfExists(
       page1,
       "Street/civic no. or house name",
       data.streetCivicNoHouseName,
       y1
     );
-    y1 = addText(
+    y1 = addTextIfExists(
       page1,
       "Apartment/unit number",
       data.apartmentUnitNumber || "",
       y1
     );
-    y1 = addText(page1, "City/town", data.cityTown || "", y1);
-    y1 = addText(page1, "District/region", data.districtRegion || "", y1);
-    y1 = addText(page1, "Country", data.country || "", y1);
+    y1 = addTextIfExists(page1, "City/town", data.cityTown || "", y1);
+    y1 = addTextIfExists(page1, "District/region", data.districtRegion || "", y1);
+    y1 = addTextIfExists(page1, "Country", data.country || "", y1);
 
     let { page: page2, y: y2 } = createPage("Employment Information");
-    y2 = addText(page2, "Occupation", data.occupation || "", y2);
-    y2 = addText(page2, "Job Title", data.jobTitle || "", y2);
-    y2 = addText(page2, "Name Of Employer", data.nameOfEmployer || "", y2);
-    y2 = addText(page2, "Employer Country", data.employerCountry || "", y2);
-    y2 = addText(page2, "Employer City", data.employerCity || "", y2);
-    y2 = addText(
+    y2 = addTextIfExists(page2, "Occupation", data.occupation || "", y2);
+    y2 = addTextIfExists(page2, "Job Title", data.jobTitle || "", y2);
+    y2 = addTextIfExists(page2, "Name Of Employer", data.nameOfEmployer || "", y2);
+    y2 = addTextIfExists(page2, "Employer Country", data.employerCountry || "", y2);
+    y2 = addTextIfExists(page2, "Employer City", data.employerCity || "", y2);
+    y2 = addTextIfExists(
       page2,
       "Employer District/region",
       data.employerDistrictRegion || "",
       y2
     );
-    y2 = addText(page2, "Since What Year", data.sinceWhatYear || "", y2);
+    y2 = addTextIfExists(page2, "Since What Year", data.sinceWhatYear || "", y2);
     y2 -= 10;
     //   let { page: page5, y: y5 } = createPage('Eligibility Questions');
     y2 = addText2(page2, "Eligibility Questions", "", y2);
     y2 -= 10;
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "* Have you ever been refused a visa or permit, denied entry to, or ordered to leave Canada \n or any other country/territory?",
       data.refusedVisa,
@@ -292,49 +299,49 @@ export default function DataTable() {
     );
     y2 -= 30;
 
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "For each refusal, please indicate the country that refused you a visa or permit, or denied \n you entry, as well as the reasons provided to you by the country",
       data.refusedVisaTextArea,
       y2
     );
     y2 -= 30;
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "* Have you ever committed, been arrested for, been charged with or convicted of any criminal \n offence in any country/territory?",
       data.criminalOffence,
       y2
     );
     y2 -= 30;
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "For each arrest, charge, or conviction, please indicate \n where (city, country), when (month/year), the nature of the offence, and the sentence.?",
       data.criminalOffenceTextArea,
       y2
     );
     y2 -= 30;
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "* In the past two years, were you diagnosed with tuberculosis or have you been in close \n contact with a person with tuberculosis?",
       data.tuberculosisDiagnosis,
       y2
     );
     y2 -= 30;
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "Is your contact with tuberculosis the result of being a health care worker?",
       data.healthcareWorkerContact,
       y2
     );
     y2 -= 30;
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "Have you ever been diagnosed with tuberculosis?",
       data.tuberculosisDiagnosed,
       y2
     );
     y2 -= 30;
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "* Do you have one of these conditions?",
       data.healthCondition,
@@ -345,26 +352,26 @@ export default function DataTable() {
     //   let { page: page6, y: y6 } = createPage('Travel Information');
     y2 = addText2(page2, "Travel Information", "", y2);
     y2 -= 10;
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "Have you ever applied for or obtained a visa, an eTA or a permit to visit, live, work or study \n in Canada?",
       data.appliedForVisa,
       y2
     );
     y2 -= 20;
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "Do you know when you will travel to Canada?",
       data.knowTravelDate,
       y2
     );
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "When do you plan to travel to Canada?",
       data.travelDate,
       y2
     );
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "Time your flight to Canada will depart",
       data.travelTime.length
@@ -376,14 +383,14 @@ export default function DataTable() {
         : "",
       y2
     );
-    y2 = addText(page2, "Are you traveling alone?", data.travelingAlone, y2);
-    y2 = addText(
+    y2 = addTextIfExists(page2, "Are you traveling alone?", data.travelingAlone, y2);
+    y2 = addTextIfExists(
       page2,
       "How many members traveling with you?",
       data.travelingMembers,
       y2
     );
-    y2 = addText(
+    y2 = addTextIfExists(
       page2,
       "Do you have Additional nationalities?",
       data.additionalNationalities,
@@ -391,24 +398,24 @@ export default function DataTable() {
     );
     y2 -= 10;
     let { page: page3, y: y3 } = createPage("Consent and Declaration");
-    // y2 = addText(page2, "Consent and Declaration", "", y2);
+    // y2 = addTextIfExists(page2, "Consent and Declaration", "", y2);
     y3 -= 10;
-    y3 = addText(
+    y3 = addTextIfExists(
       page3,
       "* Please briefly indicate if there are additional details pertinent to your application.\n For example, an urgent need to travel to Canada. \n Provide relevant details to avoid delays in the processing of your application.",
       data.additionalDetails,
       y3
     );
     y3 -= 45;
-    y3 = addText(page3, "Signature of Applicant \n", data.signature, y3);
+    y3 = addTextIfExists(page3, "Signature of Applicant \n", data.signature, y3);
     y3 -= 20;
-    y3 = addText(
+    y3 = addTextIfExists(
       page3,
       "Agree Privacy Policy, Terms and Conditions & Refund Policy",
       data.agreePrivacyPolicy,
       y3
     );
-    y3 = addText(page3, "IP Address", data.ipAddress, y3);
+    y3 = addTextIfExists(page3, "IP Address", data.ipAddress, y3);
 
     // Save the PDF to a blob
     const pdfBytes = await pdfDoc.save();
