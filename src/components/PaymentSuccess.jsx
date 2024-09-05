79ebadbb68a0dffe34aebece811973a4dbb0ebbb
bigbrandbucket-tech <162419472+bigbrandbucket-tech@users.noreply.github.com>
2024-09-05 19:20:54 +0530

    Update PaymentSuccess.jsx

--- src/components/PaymentSuccess.jsx
+++ src/components/PaymentSuccess.jsx
@@ -1,15 +1,18 @@
-import axios from "axios";
-import React, { useEffect } from "react";
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import './PaySuccess.css'; // Ensure this file contains the styling
+
 const PaymentSuccess = () => {
+  const { id } = useParams();
   return (
-    <div style={{ textAlign: "center" }}>
-      <p>
-        Payment Successfully received. Please check you mail for the transaction
-        details !!
-      </p>
-      <a href="/" style={{ color: "blue" }}>
-        Click on this link to make a new form
-      </a>
+    <div className="payment-success-container">
+      <div className="payment-success-message">
+        <h1 className="success-title">Payment Successful!</h1>
+        <p className="success-text">Your payment has been successful.</p>
+        <p className="success-text">Your ETA application Ref Number: <b>{id}</b> has been submitted.</p>
+        <p className="success-text">If you want to apply for another applicant, <Link to="/" className="apply-link">APPLY HERE</Link>.</p>
+        <Link to="/" className="return-home-link">Return to Home</Link>
+      </div>
     </div>
   );
 };
